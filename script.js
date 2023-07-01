2c1212be07c6fa5ca182df2a586a8bd25912d6d0
kian kheirani <kiankheirani@gmail.com>
2023-07-01 14:42:04 +0430

    'add_scrolling'

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const sec1 = document.querySelector('#section--1');
+const buttonscrol = document.querySelector('.btn--scroll-to');
+
+const nav_link = document.querySelectorAll('.nav__link');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -28,4 +32,16 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+buttonscrol.addEventListener('click', function (e) {
+  sec1.scrollIntoView({ behavior: 'smooth' });
+});
+nav_link.forEach(a => {
+  a.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const id = this.getAttribute('href');
+    const create = document.querySelector(id);
+    create.scrollIntoView({ behavior: 'smooth' });
+  });
 });
